be598a01a4cd606b939fa86ac614748b9b80b27a
devinmidha28 <69163361+devinmidha28@users.noreply.github.com>
2020-08-05 11:47:55 -0400

    Add files via upload

---
+++ Amusing_saver_copy_2020_08_05_15_46_43/sketch.js
@@ -0,0 +1,19 @@
+var bananaImage, obstacleImage, ObstaclesGroup, background, score;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function preload(){
+  backImage=loadImage("jungle.jpg");
+  player_running = 
+    loadAnimation("Monkey_01.png", "Monkey_02.png", "Monkey_03.png", "Monkey_04.png", "Monkey_05.png", "Monkey_06.png", "Monkey_07.png", "Monkey_08.png", "Monkey_01.png", "Monkey_01.png", "Monkey_10.png",);
+  
+  bananaImage.loadImage("Banana.png);
+  obstacleImage.loadImage("stone.png);
+                        }
+
+
+function draw() {
+  background(220);
+}
